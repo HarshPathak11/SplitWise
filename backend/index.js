7b99f88c46e607a3dfd1ff9adc587bcd134d26f3
Harsh Pathak <jkpathak83195@gmail.com>
2024-08-25 16:57:48 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import connectDB from './db/db.js';
+
+const app=express();
+
+app.use(express.json())
+app.use(cors())
+connectDB()
+
+
+
+
+
+
+
+app.listen(8000,()=>{
+    console.log('Server running on PORT:8000')
+})
