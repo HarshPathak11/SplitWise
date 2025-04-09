cdc426cac53e93fac7126ac0e9685eaab2662f78
Harsh Pathak <jkpathak83195@gmail.com>
2025-04-09 23:52:06 +0530

    basic ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import SignIn from './components/signin'
 import SignUp from './components/signup'
 import { BrowserRouter,Route,Routes,Link } from "react-router-dom";
-import Dashboard from './components/DashBoard';
+// import Dashboard from './components/DashBoard';
 import LandingPage from './components/home';
 import LandingSignin from './components/landingpage';
 import LandingSignup from './components/register';
+import ProfileEdit from './components/full-page/Profile';
+import CashMapAI from './components/full-page/CashMapAI';
+import Dashboard from './components/full-page/DashBoard';
+
 
 
 function App() {
@@ -17,8 +21,10 @@ function App() {
     <Routes>
      <Route path='/login' element={<LandingSignin/>}/>
      <Route path='/logup' element={<LandingSignup/>}/>
-     <Route path='/dash' element={<Dashboard />}/>
+     <Route path='/dash' element={<Dashboard/>}/>
      <Route path='/' element={<LandingPage />}/>
+     <Route path="/profile/edit" element={<ProfileEdit />} />
+     <Route path="/ai-assistant" element={<CashMapAI />} />
     </Routes>
     </BrowserRouter>
     </>
