331cb953b7395b0aedb42689a65ccdb7aae2deff
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2025-05-13 08:27:41 +0530

    Error message updated in add friend

--- frontend/src/components/addFriend.jsx
+++ frontend/src/components/addFriend.jsx
@@ -25,7 +25,7 @@ const AddFriend = () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (newFriendEmail.trim() === "") {
-      setError("All fields are required. Please fill in both name and email.");
+      setError("Please fill email.");
       return;
     }
 
