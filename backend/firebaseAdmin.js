c453577e158549b55fb77251f34a546f9704a853
Harsh-Pathak11 <146705265+HarshPathak11@users.noreply.github.com>
2025-08-20 00:43:40 +0530

    Update firebaseAdmin.js using env now

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,7 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
 
-const serviceAccount = require("./fairfare-d765b-firebase-adminsdk-fbsvc-2a5854aa2d.json");
+// Parse the JSON stored in environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
