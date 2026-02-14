1c8695ff01b659cee81b21031563e4a38b5b8643
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2026-02-14 16:08:12 +0530

    Fixed Username issue

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -78,6 +78,12 @@ const sendOtp = async (req, res) => {
 // Verify OTP and create user
 const verifyOtp = async (req, res) => {
   const { otp, username, email, password, otpGenerated, referId } = req.body;
+  username=username.trim();
+  email=email.trim();
+  password=password.trim();
+  otp=otp.trim();
+  otpGenerated=otpGenerated.trim();
+  referId=referId.trim();
 
   if (!otp || !email || !otpGenerated || !username || !password) {
     return res.status(400).json({ message: "Incomplete data received" });
